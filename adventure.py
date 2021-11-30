e1f84db3dade5d9d5717e1c6d710f539e4707b0a
Darence <darence.thong@gmail.com>
2021-11-30 17:09:51 -0500

    print room

---
+++ adventure.py
@@ -0,0 +1,36 @@
+from make_dungeon import MakeDungeon
+
+
+class Adventure:
+
+    def __init__(self):
+        self.p = MakeDungeon(2, 2)
+        self.p.make()
+
+    def print_room(self, x, y):
+        print(f'Current room (x: {x}, y: {y}): ')
+        print(self.p.hor[y][x] + '+\n' + self.p.ver[y][x] + self.p.ver[y][x + 1] + '\n' + self.p.hor[y + 1][x] + '+')
+
+
+    # print('exit room')
+    # print(p.hor[p.height-1][p.width-1] + '+\n' + p.ver[p.height-1][p.width-1][0] + 'X |' + '\n+--+')
+
+
+"""
+DungeonAdventure
+    • Contains the main logic for playing the game
+    • Introduces the game describing what the game is about and how to play
+    • Creates a Dungeon Object and a Adventurer Object
+    • Obtains the name of the adventurer from the user
+    • Does the following repetitively:
+        o Prints the current room (this is based on the Adventurer's current location)
+        o Determines the Adventurer's options (Move, Use a Potion)
+        o Continues this process until the Adventurer wins or dies
+        o NOTE: Include a hidden menu option for testing that prints out the entire Dungeon -- specify
+        what the menu option is in your documentation for the DungeonAdventure class
+    • At the conclusion of the game, display the entire Dungeon
+"""
+
+if __name__ == '__main__':
+    a = Adventure()
+    a.print_room(0,1)
--- make_dungeon.py
+++ make_dungeon.py
@@ -8,6 +8,37 @@ class MakeDungeon:
     def __init__(self, w=16, h=8):
         self.__width = w
         self.__height = h
+        self.__ver = []
+        self.__hor = []
+
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, w):
+        self.__width = w
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, h):
+        self.__height = h
+
+    def set_ver(self, ver):
+        self.__ver = ver
+
+    def get_ver(self):
+        return self.__ver
+
+    def set_hor(self, hor):
+        self.__hor = hor
+
+    def get_hor(self):
+        return self.__hor
+
+    width = property(get_width, set_width)
+    height = property(get_height, set_height)
+    ver = property(get_ver, set_ver)
+    hor = property(get_hor, set_hor)
 
     # place holder method for selecting difficulty, not functional
     def difficulty(self, mode):
@@ -24,18 +55,17 @@ class MakeDungeon:
         else:
             raise ValueError('Input does not match "easy", "medium", or "hard" mode options')
 
-    def make_grid(self):
+    def make(self):
         # (wide x height of 0s + last column 1s) + last row 1s
         visited = [[0] * self.__width + [1] for _ in range(self.__height)] + [[1] * (self.__width + 1)]
 
         # (wide + 1 x height) vertical walls. +[[]] allows printing of bottom wall
-        ver = [["|  "] * self.__width + ['|'] for _ in range(self.__height)] + [[]]
+        self.__ver = [["|  "] * self.__width + ['|'] for _ in range(self.__height)] + [[]]
 
         # wide (+--) + 1 (+) x height + 1) horizontal walls
-        hor = [["+--"] * self.__width + ['+'] for _ in range(self.__height + 1)]
+        self.__hor = [["+--"] * self.__width + ['+'] for _ in range(self.__height + 1)]
 
         def break_wall(x, y):
-
             # change 0 to 1 after room visited
             visited[y][x] = 1
 
@@ -53,26 +83,41 @@ class MakeDungeon:
                     continue
                 # remove horizontal wall, "+--" turns into "+  "
                 if xx == x:
-                    hor[max(y, yy)][x] = "+  "
+                    self.__hor[max(y, yy)][x] = "+  "
                 # remove vertical wall, "|"   turns into "   "
                 if yy == y:
-                    ver[y][max(x, xx)] = "   "
+                    self.__ver[y][max(x, xx)] = "   "
                 # move to next room
                 break_wall(xx, yy)
 
-        # START location
-            # randomize start location
-        # walk(randrange(w), randrange(h))
-            # specify location e.g. 0,0 for top left: x, y
+        # create & check entrance to exit pathway, else recreate
+        # while True:
+            # Entrance location
+                # randomize start location (OPTION)
+            # walk(randrange(w), randrange(h))
+                # specify location e.g. 0,0 for top left: x, y
         break_wall(1, 0)
 
-        # print maze
+            # if self.traverse_dungeon():
+            #     break
+
+    def traverse_dungeon(self):
+        exit = (self.__width - 1, self.__height - 1)    # bottom right corner
+
+        while True:
+            pass
+
+    def print_dungeon(self):
         s = ""
-        for (a, b) in zip(hor, ver):
+        for (a, b) in zip(self.__hor, self.__ver):
             s += ''.join(a + ['\n'] + b + ['\n'])
         return s
 
+    def __str__(self):
+        return self.print_dungeon()
+
 
 if __name__ == '__main__':
-    p = MakeDungeon(2, 2)
-    print(p.make_grid())
+    p = MakeDungeon(8, 4)
+    p.make()
+    print(p)
