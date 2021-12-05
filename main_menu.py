bbde390a29ff177248f34664af937355b8b41cf8
Procrastinav <94326046+Procrastinav@users.noreply.github.com>
2021-12-05 12:48:50 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,29 +1,34 @@
-from instructions import instructions
-from instructions import CharacterCreation
+#from character_creation import CharacterCreation
 
 
 def menu():
-    print("[1] Start New Game")
-    print("[2] Game Instructions")
-    print("[0] Exit Game")
+    print("[1] Start New Game\n[2] Game Instructions\n[0] Exit Game")
 
 
-menu()
-option = int(input("Enter the option you would like to choose: "))
-while option != 0:
-    if option == 1:
-#        obj = instructions.CharacterCreation
+def instructions():
+    print("Add Instructions Here")
 
-    elif option == 2:
-        instructions()
-        print()
-        menu()
-        option = int(input("Enter the option you would like to choose: "))
-    else:
-        print("Invalid Option! Please choose again.")
 
-        print()
-        menu()
-        option = int(input("Enter the option you would like to choose: "))
+class MainMenu:
+    menu()
+    option = int(input("Enter the option you would like to choose: "))
+    while option != 0:
+        if option == 1:
+            pass
+        elif option == 2:
+            instructions()
+            print()
+            menu()
+            option = int(input("Enter the option you would like to choose: "))
+        else:
+            print("Invalid Option! Please choose again.")
 
-print("Thank you for playing!")
+            print()
+            menu()
+            option = int(input("Enter the option you would like to choose: "))
+
+    print("Thank you for playing!")
+
+
+if __name__ == '__main__':
+    m = MainMenu()
